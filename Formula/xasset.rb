354e7aea53470e2a20cc26b02b6f9cc0e8b81d72
Mahmudul Hasan <54240258+mhasan341@users.noreply.github.com>
2023-12-14 02:35:29 +0600

    Update xasset.rb

--- Formula/xasset.rb
+++ Formula/xasset.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Xasset < Formula
-    desc "`xlist` is a command-line tool to list folders with `.imageset` or `.colorset` extensions in Xcode asset catalogs."
+    desc "`xasset` is a command-line tool to list folders with `.imageset` or `.colorset` extensions in Xcode asset catalogs."
   homepage "https://github.com/mhasan341/homebrew-xasset"
   url "https://github.com/mhasan341/homebrew-xasset/releases/download/v1.0.0/xasset.sh"
   sha256 "ba85409ad0e51dc9872fafc406c45c0d691cf9354f39bd5e84391ffe12b886e0"
@@ -10,7 +10,7 @@ class Xasset < Formula
 
 
   def install
-      bin.install "xasset"
+      bin.install "xasset.sh" => "xasset"
   end
 
   test do
